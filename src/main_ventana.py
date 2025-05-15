7cab46f5b9455b0a473c7f99180cd4432e2af5a0
LuisDCS <1001.27539960.ucla@gmail.com>
2025-05-15 13:41:23 -0400

    Cambios menores

--- src/main_ventana.py
+++ src/main_ventana.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import subprocess
+import os
 from vista_pilas import InscripcionesApp
 
 # Funciones para los botones
@@ -13,6 +14,12 @@ def abrir_pilas():
 
 def abrir_colas():
     subprocess.run(["python", "src/vista_colas.py"])
+
+# Función para borrar los datos de estudiantes inscritos
+def borrar_datos_estudiantes():
+    temp_file = "temp_estudiantes.json"
+    if os.path.exists(temp_file):
+        os.remove(temp_file)
 
 # Crear ventana principal
 ventana = tk.Tk()
@@ -28,6 +35,9 @@ btn_colas = tk.Button(ventana, text="Colas", command=abrir_colas)
 btn_listas.pack(pady=10)
 btn_pilas.pack(pady=10)
 btn_colas.pack(pady=10)
+
+# Vincular la función al evento de cierre de la ventana principal
+ventana.protocol("WM_DELETE_WINDOW", lambda: (borrar_datos_estudiantes(), ventana.destroy()))
 
 # Ejecutar ventana principal
 ventana.mainloop()
